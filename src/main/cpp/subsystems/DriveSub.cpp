a868866b0b7b394b3afa0448418f96b618c3419f
ZhangzrJerry <212563134@qq.com>
2022-08-25 00:04:08 +0800

    umm...a little progress

--- src/main/cpp/subsystems/DriveSub.cpp
+++ src/main/cpp/subsystems/DriveSub.cpp
@@ -4,13 +4,41 @@
 DriveSub::DriveSub(){
     left = new ctre::phoenix::motorcontrol::can::TalonSRX(1);
     left_follow = new ctre::phoenix::motorcontrol::can::TalonSRX(2);
+    left_follow->Follow(&left,ctre::phoenix::motorcontrol::FollowerType::FollowerType_PercentOutput);
     rght = new ctre::phoenix::motorcontrol::can::TalonSRX(3);
     rght_follow = new ctre::phoenix::motorcontrol::can::TalonSRX(4);
-    left_follow->Follow(&left,ctre::phoenix::motorcontrol::FollowerType::FollowerType_PercentOutput);
     rght_follow->Follow(&rght,ctre::phoenix::motorcontrol::FollowerType::FollowerType_PercentOutput);
 }
 
 
 DriveSub::~DriveSub(){
 
+}
+
+void DriveSub::drive(double x, double w, double max_percent){
+    double left_speed,rght_speed;
+    if(x>0){
+        left_speed = x + w;
+        rght_speed = x - w;
+    }else{
+        left_speed = x - w;
+        rght_speed = x + w;
+    }
+    double max_speed = left_speed > rght_speed ? left_speed : rght_speed;
+    double k_speed = max_percent / max_speed;
+    left->Set(ControlMode::PercentOutput,+direct*left_speed*k_speed);
+    rght->Set(ControlMode::PercentOutput,-direct*rght_speed*k_speed);
+    return;
+}
+
+void DriveSub::auto_drive(int code){
+    if(code=="left"){
+
+    }else if(code=="rght"){
+        
+    }else if(code=="back"){
+-
+    }else{
+
+    }
 }
--- src/main/include/subsystems/DriveSub.h
+++ src/main/include/subsystems/DriveSub.h
@@ -1,13 +1,25 @@
 #pragma once
 #include <ctre/phoenix/motorcontrol/can/TalonSRX.h>
+#include <cmath>
 class DriveSub{
  public:
-    DriveSub();
-    ~DriveSub();
-    void drive();
+   DriveSub();
+   ~DriveSub();
+   void drive(double x, double w, double max_percent);
+   /**
+    * @brief 自动阶段程序
+    * @param
+    * @param code "left","rght","back","stay&back:stay out of this function"
+    */
+   void auto_drive(int code);
  private:
-    ctre::phoenix::motorcontrol::can::TalonSRX* left;
-    ctre::phoenix::motorcontrol::can::TalonSRX* left_follow;
-    ctre::phoenix::motorcontrol::can::TalonSRX* rght;
-    ctre::phoenix::motorcontrol::can::TalonSRX* rght_follow;
+   /**
+    * @brief 机器人方向
+    * @param +1或-1
+    */
+   int direct = +1; 
+   ctre::phoenix::motorcontrol::can::TalonSRX* left;
+   ctre::phoenix::motorcontrol::can::TalonSRX* left_follow;
+   ctre::phoenix::motorcontrol::can::TalonSRX* rght;
+   ctre::phoenix::motorcontrol::can::TalonSRX* rght_follow;
 };
