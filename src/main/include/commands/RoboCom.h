8cd960b180d06a8eacf22aed39edaf12da8ffce7
ZhangzrJerry <212563134@qq.com>
2022-08-27 16:10:22 +0800

    RoboCom contain X

---
+++ src/main/include/commands/RoboCom.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "../subsystems/ClimbSub.h"
+#include "../subsystems/CollectSub.h"
+#include "../subsystems/DriveSub.h"
+#include <frc/Joystick.h>
+
+class RoboCom{
+ public:
+    RoboCom();
+    ~RoboCom();
+    void init_com();
+    void auto_com();
+    void tele_com();
+ private:
+    double filter(double x);
+    frc::Joystick stick{0};
+    /**
+     * 爬升子系统
+     */
+    ClimbSub CSub{};
+    /**
+     * 收球子系统
+     */
+    CollectSub FSub{};
+    /**
+     * 底盘子系统
+     */
+    DriveSub DSub{};
+};
